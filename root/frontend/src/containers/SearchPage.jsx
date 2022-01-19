9abbdcd55f966a402e4370db9ea88679f88d2046
AndrewMiddletonBC <andrew.middleton@bellevuecollege.edu>
2022-01-18 23:18:32 -0800

    completed react landing page demo

---
+++ root/frontend/src/containers/SearchPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ContentContainer from '../components/ContentContainer'
+
+const SearchPage = () => {
+  return (
+    <ContentContainer>
+      <h1>Search Page Content</h1>
+    </ContentContainer>
+  );
+}
+
+export default SearchPage;
