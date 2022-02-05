49519e1592344d6e65e260ac9f9f933a95268e63
AndrewMiddletonBC <andrew.middleton@bellevuecollege.edu>
2022-02-05 15:27:48 -0800

    attempt to fix incompatibility issues

--- root/frontend/src/pages/PreviewPage.jsx
+++ root/frontend/src/pages/PreviewPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components'
-import JSONPretty from 'react-json-pretty';
+
 const PreviewPage = (props) => {
 
   return(
     <div>
-      <JSONPretty id="json-pretty" data={props.result}></JSONPretty>
+      {JSON.stringify(props.result)}
     </div>
   )
 }
