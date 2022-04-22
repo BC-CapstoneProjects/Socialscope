9de8c17d4067c4082e0cd94283e38b4dcbbbc10f
thienhuynh98 <thien.huynh@bellevuecollege.edu>
2022-04-22 13:27:18 -0700

    test

--- root/backend/src/main/java/util/Credentials.java
+++ root/backend/src/main/java/util/Credentials.java
@@ -1,14 +1,8 @@
 package util;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.language.v1.LanguageServiceSettings;
@@ -17,7 +11,7 @@ import org.json.JSONObject;
 
 public class Credentials {
 
-	private static final String DEFAULT_CREDENTIALS_FILE = "private/credentials.json";
+//	private static final String DEFAULT_CREDENTIALS_FILE = "private/credentials.json";
     private static final String DEFAULT_CREDENTIALS_GOOGLEFILE ="private/socialsentanalysis.json";
 
     private String redditAppId;
@@ -32,17 +26,25 @@ public class Credentials {
     
     private LanguageServiceSettings googleLanguageServiceSettings;
 
+//    public Credentials() {
+//        readCredentialsFromFile(DEFAULT_CREDENTIALS_FILE);
+//        readGoogleCredentials(DEFAULT_CREDENTIALS_GOOGLEFILE);
+//    }
+
     public Credentials() {
-        readCredentialsFromFile(DEFAULT_CREDENTIALS_FILE);
+        readCredentialsFromFile();
         readGoogleCredentials(DEFAULT_CREDENTIALS_GOOGLEFILE);
     }
     
     public void readGoogleCredentials(String fp) {
     	try {
     		InputStream in = getClass().getResourceAsStream("/" + fp); // in jar
-    		if (in == null) 
+    		if (in == null)
     			in = new FileInputStream("src/main/resources/" + fp); // not in jar
-    		
+            if(in == null)
+            {
+                in = getClass().getResourceAsStream("secret/socialsentanalysis.json"); // in jar
+            }
     		this.googleLanguageServiceSettings =
     			LanguageServiceSettings.newBuilder()
                 	.setCredentialsProvider(FixedCredentialsProvider.create(ServiceAccountCredentials.fromStream(in)))
@@ -51,40 +53,77 @@ public class Credentials {
     		e.printStackTrace();
     	}
     }
-    
-    private void readCredentialsFromFile(String fp) {
-        BufferedReader reader;
-        try {
-        	String currPath = new java.io.File(".").getCanonicalPath();
-        	System.out.println(currPath);
-        	InputStream in = getClass().getResourceAsStream("/" + fp);  
-        	if (in != null)
-        		reader = new BufferedReader(new InputStreamReader(in)); // read file from inside jar 
-        	else
-        		reader = new BufferedReader(new FileReader("src/main/resources/" + fp)); // read while not in jar
-//                reader = new BufferedReader(new FileReader(fp));
-            StringBuilder sb = new StringBuilder();
-            reader.lines().forEach((String line) -> sb.append(line));
-            reader.close();
-            JSONObject jo = new JSONObject(sb.toString());
-            this.redditAppId = jo.getString("redditAppId");
-            this.redditAppSecret = jo.getString("redditAppSecret");
-            this.redditUserAgent = jo.getString("redditUserAgent");
-            this.twitterAppId = jo.getString("twitterAppId");
-            this.twitterAppSecret = jo.getString("twitterAppSecret");
-            this.twitterUserAgent = jo.getString("twitterUserAgent");
-            this.youtubeUserAgent = jo.getString("youtubeUserAgent");
-            this.youtubeApiKey = jo.getString("youtubeApiKey");
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+//
+//    private void readCredentialsFromFile(String fp) {
+//        BufferedReader reader;
+//        try {
+//        	String currPath = new java.io.File(".").getCanonicalPath();
+//        	System.out.println(currPath);
+//        	InputStream in = getClass().getResourceAsStream("/" + fp);
+//        	if (in != null)
+//        		reader = new BufferedReader(new InputStreamReader(in)); // read file from inside jar
+//        	else
+//        		reader = new BufferedReader(new FileReader("src/main/resources/" + fp)); // read while not in jar
+////                reader = new BufferedReader(new FileReader(fp));
+//            StringBuilder sb = new StringBuilder();
+//            reader.lines().forEach((String line) -> sb.append(line));
+//            reader.close();
+//            JSONObject jo = new JSONObject(sb.toString());
+//            this.redditAppId = jo.getString("redditAppId");
+//            this.redditAppSecret = jo.getString("redditAppSecret");
+//            this.redditUserAgent = jo.getString("redditUserAgent");
+//            this.twitterAppId = jo.getString("twitterAppId");
+//            this.twitterAppSecret = jo.getString("twitterAppSecret");
+//            this.twitterUserAgent = jo.getString("twitterUserAgent");
+//            this.youtubeUserAgent = jo.getString("youtubeUserAgent");
+//            this.youtubeApiKey = jo.getString("youtubeApiKey");
+//        } catch (FileNotFoundException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } catch (JSONException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//    }
+//    public void readGoogleCredentials() {
+//        try {
+//            JSONObject google = new JSONObject();
+//            google.put("type", System.getenv("type"));
+//            google.put("project_id", System.getenv("project_id"));
+//            google.put("private_key_id", System.getenv("private_key_id"));
+//            google.put("private_key", System.getenv("private_key"));
+//            google.put("client_email", System.getenv("client_email"));
+//            google.put("client_id", System.getenv("client_id"));
+//            google.put("auth_uri", System.getenv("auth_uri"));
+//            google.put("token_uri", System.getenv("token_uri"));
+//            google.put("auth_provider_x509_cert_url", System.getenv("auth_provider_x509_cert_url"));
+//            google.put("client_x509_cert_url", System.getenv("client_x509_cert_url"));
+//
+//            String googleCredential = google.toString();
+//            System.out.println(googleCredential);
+//            InputStream in = new ByteArrayInputStream(googleCredential.getBytes(StandardCharsets.UTF_8));
+//            System.out.println(in);
+//            this.googleLanguageServiceSettings =
+//                    LanguageServiceSettings.newBuilder()
+//                            .setCredentialsProvider(FixedCredentialsProvider.create(ServiceAccountCredentials.fromStream(in)))
+//                            .build();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    private void readCredentialsFromFile() {
+        this.redditAppId = System.getenv("redditAppId");
+        this.redditAppSecret = System.getenv("redditAppSecret");
+        this.redditUserAgent = System.getenv("redditUserAgent");
+        this.twitterAppId = System.getenv("twitterAppId");
+        this.twitterAppSecret = System.getenv("twitterAppSecret");
+        this.twitterUserAgent = System.getenv("twitterUserAgent");
+        this.youtubeUserAgent = System.getenv("youtubeUserAgent");
+        this.youtubeApiKey = System.getenv("youtubeApiKey");
     }
 
     @SuppressWarnings("unused")
